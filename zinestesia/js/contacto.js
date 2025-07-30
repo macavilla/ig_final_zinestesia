1b056c3a802333d1193de6bac243bb88efd44a1c
macavilla <maca.villa.a@gmail.com>
2025-07-30 18:37:18 -0300

    agrego validacion

--- zinestesia/js/contacto.js
+++ zinestesia/js/contacto.js
@@ -4,9 +4,32 @@ const form = document.getElementById("contact-form");
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const messageInput = document.getElementById("message");
+const emailError = document.getElementById("email-error");
+const submitBtn = document.getElementById("submit-btn");
 
-emailInput.addEventListener("change", () => {
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+console.log("emailInput :>> ", emailInput);
+const validationMessages = {
+  empty: "Ingrese un mail.",
+  invalid: "El mail ingresado no es válido, por favor reviselo.",
+};
+
+emailInput.addEventListener("input", () => {
   const emailValue = emailInput.value;
-  if (emailValue.length > 0) {
+
+  if (emailValue === "") {
+    emailError.style.display = "inline";
+    emailError.innerHTML = validationMessages.empty;
+    submitBtn.disabled = true;
+    return;
+  }
+  if (emailRegex.test(emailValue)) {
+    emailError.style.display = "none";
+    submitBtn.disabled = false;
+  } else {
+    emailError.style.display = "inline";
+    emailError.innerHTML = validationMessages.invalid;
+    submitBtn.disabled = true;
   }
 });
