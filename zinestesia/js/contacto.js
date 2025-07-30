b0c347b23814e743b51805f881902def68ce9e7d
macavilla <maca.villa.a@gmail.com>
2025-07-30 17:34:37 -0300

    contact form

---
+++ zinestesia/js/contacto.js
@@ -0,0 +1,12 @@
+console.log("contacto :>> ");
+
+const form = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+emailInput.addEventListener("change", () => {
+  const emailValue = emailInput.value;
+  if (emailValue.length > 0) {
+  }
+});
